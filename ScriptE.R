e9e8555d4fea9340b80600d1ab720eba482fea8c
Estuardo07 <xavihernand7@hotmail.com>
2022-07-23 13:58:25 -0600

    Update

--- ScriptE.R
+++ ScriptE.R
@@ -1,8 +1,30 @@
-library(tibbletime)
-library(dplyr)
-library(tidyverse)
 
 
+#Importar el dataset
 db<-read.csv('train.csv')
-
+#Resumen del dataset
 summary(db)
+
+
+#Gráficos de variables continuas
+hist(x = db$LotFrontage)
+hist(x = db$LotArea)
+hist(x = db$MasVnrArea)
+hist(x = db$BsmtFinSF1)
+hist(x = db$BsmtFinSF2)
+hist(x = db$BsmtUnfSF)
+hist(x = db$TotalBsmtSF)
+hist(x = db$X1stFlrSF)
+hist(x = db$X2ndFlrSF)
+hist(x = db$LowQualFinSF)
+hist(x = db$GrLivArea)
+hist(x = db$GarageArea)
+hist(x = db$WoodDeckSF)
+hist(x = db$OpenPorchSF)
+hist(x = db$EnclosedPorch)
+hist(x = db$ScreenPorch)
+hist(x = db$PoolArea)
+hist(x = db$MiscVal)
+
+
+#Gráficos de variables discretas
